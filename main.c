1816521fce554e1c9844c37bc1983871ecebaad4
conalex <alexcon314@gmail.com>
2018-05-07 06:17:03 +0300

    fix names

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ struct termios tty_orig;
 static void             /* Reset terminal mode on program exit */
 tty_reset(void)
 {
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty_orig) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty_orig) == -1) {
         perror("tcsetattr");
+		exit(EXIT_FAILURE);
+	}
 }
 
 FILE* unbuff_popen(char* cmd, char* arg[], char* env[]) {
@@ -22,21 +24,28 @@ FILE* unbuff_popen(char* cmd, char* arg[], char* env[]) {
     int pty;
     struct winsize ws;
     
-    if (tcgetattr(STDIN_FILENO, &tty_orig) == -1)
+    if (tcgetattr(STDIN_FILENO, &tty_orig) == -1) {
         perror("tcgetattr");
-    if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char *) &ws) < 0)
+		return NULL;
+	}
+    if (ioctl(STDIN_FILENO, TIOCGWINSZ, (char *) &ws) < 0) {
         perror("TIOCGWINSZ error");
+		return NULL;
+	}
 
     switch (forkpty(&pty, NULL, &tty_orig, &ws)) {
         case -1:
             perror("pty fork");
+			return NULL;
         case 0:
-                execve(cmd, arg, env);
-                perror("execve");
+            execve(cmd, arg, env);
+            perror("execve");
         default:    /* Parent*/
 
-            if (atexit(tty_reset) != 0)
+            if (atexit(tty_reset) != 0) {
                 perror("atexit");
+				return NULL;
+			}
 
             return fdopen(pty, "r");
     }
@@ -48,6 +57,9 @@ main(int argc, char *argv[])
     char *arg[] = {argv[1], argv[2], NULL };
     char *env[] = { NULL };
     FILE* fp = unbuff_popen(argv[1], arg, env);
-    while(fgets( buf, BUF_SIZE, fp))
-        printf("out: %s", buf);
+	if(fp) {
+    	while(fgets( buf, BUF_SIZE, fp))
+        	printf("out: %s", buf);
+	}
+	return 0;
 }
