c5603b60e83f458a3181cd02dc7debd3f0b49e56
conalex <alexcon314@gmail.com>
2018-06-19 11:36:20 +0300

    zm zec

--- re/main.c
+++ re/main.c
@@ -79,7 +79,7 @@ zm(void) {
 
 //
     memset(&result, 0, sizeof result);
-    str = "2018-06-15 05:08:18 PM|>  ============== |  295.1 Sol/s   295.1 Avg   158.5 I/s | 2.82 S/W  105 W |  0.49  100  80 ++";
+    str = "2018-06-15 05:08:18 PM|   ============== |  295.1 Sol/s   295.1 Avg   158.5 I/s | 2.82 S/W  105 W |  0.49  100  80 ++";
     re = // 16 fields
     "([0-9]{4}-[0-9]{2}-[0-9]{2}\\s+[0-9]{2}:[0-9]{2}:[0-9]{2})\\s+"
     "([PA]M)"
@@ -113,35 +113,58 @@ zec(void) {
     //
     memset(&result, 0, sizeof result);
     str = "INFO: Current pool: eu1-zcash.flypool.org:3333";
-    re = "^INFO:.+(pool):\\W+(.+)\\W?";
+    re = // 3 fields
+    "^INFO:.+(pool):\\s+"
+    "(.+)\\W?";
 
     n = re_match(re, str, result);
     print_result(result, n);
     //
     memset(&result, 0, sizeof result);
     str = "CUDA: Device: 0 GeForce GTX 1060 3GB, 3019 MB i:64";
-    re = "^CUDA:.+Device:\\s([0-9]{1,2})\\s(.+GTX\\s)([0-9]+)\\s([0-9]+GB).+\\W?";
+    re = // 5 fields
+    "^CUDA:.+Device:\\s"
+    "([0-9]+)\\s"
+    "(.+GTX\\s)"
+    "([0-9]+)\\s"
+    "([0-9]+GB)\\W?";
 
     n = re_match(re, str, result);
     print_result(result, n);
     //
     memset(&result, 0, sizeof result);
     str = "GPU0: 277 Sol/s GPU1: 178 Sol/s ";
-    re = "^GPU([0-9]+):\\s([0-9]+)\\s(\\w+\\W?\\w+)\\s+GPU([0-9]+):\\s([0-9]+)\\s(\\w+\\W?\\w+)\\W?";
+    re = // 7 fields
+    "^GPU"
+    "([0-9]+):\\s"
+    "([0-9]+)\\s"
+    "(\\w+\\W?\\w+)\\s+"
+    "GPU([0-9]+):\\s"
+    "([0-9]+)\\s"
+    "(\\w+\\W?\\w+)\\W?";
 
     n = re_match(re, str, result);
     print_result(result, n);
     //
     memset(&result, 0, sizeof result);
     str = "Total speed: 455 Sol/s";
-    re = "^Total speed:\\s([0-9]+)\\s(\\w+\\W?\\w+)\\W?";
+    re = // 3 fields
+    "^Total speed:\\s"
+    "([0-9]+)\\s"
+    "(\\w+\\W?\\w+)\\W?";
 
     n = re_match(re, str, result);
     print_result(result, n);
     //
     memset(&result, 0, sizeof result);
     str = "|  0  |    107W     |  2.59 Sol/W  |";
-    re = "^\\|\\s+([0-9]+)\\s+\\|\\s+([0-9]+)(\\w+)\\s+\\|\\s+([0-9]+\\.[0-9]+)\\s(\\w+\\W?\\w+)\\s+\\W?";
+    re = // 6 fields
+    "^"
+    "\\|\\s+([0-9]+)\\s+"
+    "\\|\\s+([0-9]+)(\\w+)\\s+"
+    "\\|\\s+([0-9]+\\.[0-9]+)\\s"
+    "(\\w+\\W?\\w+)\\s+"
+    "\\|\\W?";
 
     n = re_match(re, str, result);
     print_result(result, n);
@@ -151,6 +174,5 @@ int
 main() {
     zm();
     zec();
-    return 0;
-    
+    return 0;    
 }
