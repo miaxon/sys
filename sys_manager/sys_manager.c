c0e7f507fe0cc92369f8519160a40759b196f2a4
conalex <alexcon314@gmail.com>
2018-07-03 23:55:31 +0300

    add net lib

---
+++ sys_manager/sys_manager.c
@@ -0,0 +1,21 @@
+#include <sys/sysinfo.h>
+#include <unistd.h>
+#include "sys_manager.h"
+
+int
+sys_manager_init(void) {
+	int err = 0;
+	return err;
+}
+
+int
+sys_manager_destroy(void) {
+	int err = 0;
+	return err;
+}
+
+int
+sys_manager_update(void) {
+	int err = 0;
+	return err;
+}
---
+++ sys_manager/sys_manager.h
@@ -0,0 +1,15 @@
+
+#ifndef SYS_MANAGER_H
+#define SYS_MANAGER_H
+
+int
+sys_manager_init(void);
+
+int
+sys_manager_destroy(void);
+
+int
+sys_manager_update(void);
+
+#endif /* SYS_MANAGER_H */
+
